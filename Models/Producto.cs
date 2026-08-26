16a6e51e196623972f617a95e4ed8ec0f32c7f67
Mariela García Tejada <marielagarciatejada@gmail.com>
2026-08-26 13:05:44 -0400

    Cadena de conexión de Somee Y Creación de la conexión a la base de datos de Some mediante Dapper

---
+++ Models/Producto.cs
@@ -0,0 +1,12 @@
+namespace MiApiCuadrado.Api.Models
+{
+   public class Producto
+    {
+        public int Id_Producto {get; set;}
+        public string Nombre {get; set;}
+        public string? Descripcion {get; set;}
+        public decimal Precio {get; set;}
+        public int Stock {get; set;}
+        public DateTime FechaRegistro{get; set;}
+    }
+}
